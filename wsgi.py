0c51f79fcc8c92a5cbd81d0fb8b32593de903607
Chance Zibolski <chance.zibolski@gmail.com>
2013-05-30 14:37:20 -0700

    Add wsgi file. Add gunicorn as requirement.

---
+++ wsgi.py
@@ -0,0 +1,13 @@
+# activate virtual environment
+activate_this = '%s/bin/activate_this.py' % "."
+execfile(activate_this, dict(__file__=activate_this))
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+application = newrelic.agent.wsgi_application()(application)
